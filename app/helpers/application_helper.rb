73286940d0a4e434ff4c8c9f7eda86d86d899285
ujkhokhar <ujkhokhar@gmail.com>
2015-05-13 02:26:13 -0400

    Display works on home page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def sort_works_into_rows(works)
+    works.each_slice(3).map do |group|
+      content_tag :div, class: 'row' do
+        group.map do |work|
+          content_tag :div, class: 'col-md-3, col-lg-3, col-xs-3' do
+            link_to "/works/#{work.id}" do
+              image_tag(work.screenshots.thumb.url)
+            end
+          end
+        end.join.html_safe
+      end
+    end.join.html_safe
+  end
+  
 end
