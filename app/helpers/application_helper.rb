39632ee97ef272ec4da0c484b6e3596ad5067b6d
ujkhokhar <ujkhokhar@gmail.com>
2015-05-27 20:21:44 -0400

    [CSS] - Style Changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
   def sort_works_into_rows(works)
-    works.each_slice(3).map do |group|
+    works.each_slice(4).map do |group|
       content_tag :div, class: 'row' do
         group.map do |work|
-          content_tag :div, class: 'col-md-3, col-lg-3, col-xs-3' do
+          content_tag :div, class: 'work-col col-lg-3 col-md-6 col-xs-12' do
             link_to "/works/#{work.id}" do
-              image_tag(work.screenshots.thumb.url)
+              image_tag("https://ujk-portfolio-development.s3.amazonaws.com/uploads/work/#{work.name}/#{work.name}.svg", class: 'work-img')
             end
           end
         end.join.html_safe
